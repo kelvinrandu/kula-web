05679f004c9f2fa028742f992a7905d3a5eaab7f
kelvinrandu <randukelvin@gmail.com>
2021-11-11 09:58:08 +0300

    added login,logout function and dashboard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,19 @@
-import React, { useContext } from "react";
+import React, { useEffect, useContext } from "react";
 import type { NextPage } from "next";
+import { Text, Flex, Spinner } from "@chakra-ui/react";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Login from "../components/Login";
 import { Center } from "@chakra-ui/layout";
 import { AuthContext } from "../context/AuthContext";
+import Router from "next/router";
 
 const Home: NextPage = () => {
   const user = useContext(AuthContext);
+  useEffect(() => {
+    if (user) Router.push("/dashboard");
+  }, [user]);
 
   return (
     <>
@@ -17,7 +22,10 @@ const Home: NextPage = () => {
           <Login />
         </Center>
       ) : (
-        <h2 className="mt-4 text-center">Welcome {user.email}</h2>
+                <Flex pt={24} align="center" justify="center">
+          <Spinner size="xl" label="Loading outbox" />
+        </Flex>
+       
       )}
     </>
   );
