fdb51fd65e8ebf44b0c53542aa104e334c9b48c4
kelvinrandu <randukelvin@gmail.com>
2021-12-21 11:59:56 +0300

    restructered layout

---
+++ src/components/AddItemModal.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AddItemModal() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/components/OrderSingle.tsx
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import { Collapse, Badge } from "@chakra-ui/react";
+import { Box, Heading, Text, Flex, Spacer } from "@chakra-ui/layout";
+import { TriangleDownIcon, TriangleUpIcon, AtSignIcon } from "@chakra-ui/icons";
+// import ItemDetail from "../components/ItemDetail";
+// import EditItem from "../components/EditItem";
+
+export type ItemProps = {
+  id: number;
+  name: string;
+  price: number;
+  user: {
+    name: string;
+    email: string;
+    auth0_id: string;
+  };
+  category: {
+    name: string;
+    description: string;
+  };
+};
+interface Props {
+  item?: ItemProps;
+  myItem?: Boolean;
+}
+
+const badgeColors = {
+  food: "teal",
+  drinks: "red",
+  hardware: "blue",
+  textile: "orange",
+  electronics: "yellow",
+};
+
+const ItemSingle: React.FC<Props> = () => {
+  const [itemDetail, setItemDetail] = useState(false);
+
+  function ItemDetailHandler() {
+    setItemDetail(!itemDetail);
+  }
+  return (
+    <Box
+      _hover={{
+        transform: "translateY(-3px)",
+        boxShadow: "dark-lg",
+      }}
+      borderWidth="1px"
+      borderRadius={8}
+      mb={2}
+      p={8}
+      shadow="md"
+      margin={2}
+    >
+      <Flex justify="center" align="center" wrap="wrap" grow={1}>
+        <Heading fontSize="md" mr={4}>
+          number
+        </Heading>
+        {/* <Spacer /> */}
+        <AtSignIcon color="teal" />
+        <Text> Ksh 100 </Text>
+
+        <Spacer />
+
+
+            <Badge mr={5} colorScheme='red'>
+              category
+            </Badge>
+
+            <Box
+              as="button"
+              alignSelf="right"
+              float="right"
+              onClick={() => ItemDetailHandler()}
+            >
+      
+                <TriangleDownIcon color="teal" boxSize={6} />
+         
+            </Box>
+        
+        
+      </Flex>
+
+    </Box>
+  );
+};
+
+export default ItemSingle;
