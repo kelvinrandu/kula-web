e17f1e89a5b6388b789e98193755cfc2fc39bed8
kelvinrandu <randukelvin@gmail.com>
2023-05-01 19:55:22 +0300

    added add transactions to db

--- src/pages/api/callback.ts
+++ src/pages/api/callback.ts
@@ -1,25 +1,63 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { Mpesa } from "mpesa-api";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  DocumentData,
+  getDocs,
+  setDoc,
+  limit,
+  query,
+  QueryDocumentSnapshot,
+  updateDoc,
+  where,
+} from "@firebase/firestore";
+import { Firestore } from "../../firebase";
+
 
 type Data = {
   name: string;
   callback: string;
 };
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-    if (req.method === "POST") {
-      // Process a POST request
-      const callbackData = req.body;
-      console.log('callback -->',callbackData)
-       res.status(200).json({ callback: callbackData , name: "John Doe" });
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<Data>
+// ) {
+//     if (req.method === "POST") {
+//       // Process a POST request
+//       const callbackData = req.body;
+//       console.log('callback -->',callbackData)
+//       await setDoc(doc(db, "cities", "LA"), {
+//         name: "Los Angeles",
+//         state: "CA",
+//         country: "USA",
+//       });
+//        res.status(200).json({ callback: callbackData , name: "John Doe" });
 
-    } else {
-      // Handle any other HTTP method
-    }
+//     } else {
+//       // Handle any other HTTP method
+//     }
 
  
-}
+// }
+export default async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+      if (req.method === "POST") {
+        // Process a POST request
+        const callbackData = req.body;
+        console.log('callback -->',callbackData)
+     await setDoc(doc(Firestore, "orders", "LA"), {
+       name: req.body,
+       state: "CA",
+       country: "USA",
+     });
+         res.status(200).json({ callback: callbackData , name: "John 1Doe" });
+
+      } else {
+        // Handle any other HTTP method
+      }
+
+ 
+};
