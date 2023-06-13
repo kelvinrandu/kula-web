fd1730caefdf6e8bfa8a6044d8ef19b556e3475e
kelvinrandu <randukelvin@gmail.com>
2023-06-13 15:38:24 +0300

    added form wizard

--- src/components/AddItemModal.tsx
+++ src/components/AddItemModal.tsx
@@ -17,6 +17,17 @@ import {
   Button,
   Flex,
   useToast,
+  Progress,
+  Box,
+  ButtonGroup,
+  Heading,
+  GridItem,
+  SimpleGrid,
+  InputLeftAddon,
+  InputGroup,
+  Textarea,
+  FormHelperText,
+  InputRightElement,
 } from "@chakra-ui/react";
 import { doc, setDoc, collection } from "firebase/firestore";
 import {
@@ -45,39 +56,241 @@ interface RestaurantData {
   image_url: string;
 }
 interface Props {}
+const Form1 = () => {
+  const [show, setShow] = React.useState(false);
+  const [selectedOptions, setSelectedOptions] = useState();
+  const [imageAsFile, setImageAsFile] = useState<Uint8Array | undefined>();
+  const handleClick = () => setShow(!show);
+  const handleImageAsFile = (e: any) => {
+    const image = e.target.files[0];
+    setImageAsFile((imageFile) => image);
+  };
+
+  function handleSelect(data: any) {
+    setSelectedOptions(data);
+  }
+  return (
+    <>
+      <Heading w="100%" textAlign={"center"} fontWeight="normal" mb="2%">
+         Details
+      </Heading>
+
+      <FormControl mr="5%">
+        <FormLabel htmlFor="first-name" fontWeight={"normal"}>
+          Restaurant name
+        </FormLabel>
+        <Input id="first-name" placeholder="Restaurant name" />
+      </FormControl>
+
+      <FormControl>
+        <FormLabel htmlFor="last-name" fontWeight={"normal"}>
+          Category
+        </FormLabel>
+        <Creatable
+          value={selectedOptions}
+          onChange={handleSelect}
+          isMulti={true}
+          options={options}
+        />
+      </FormControl>
+
+      <FormControl mt="2%">
+        <FormLabel htmlFor="email" fontWeight={"normal"}>
+          Tags
+        </FormLabel>
+        <Creatable
+          value={selectedOptions}
+          onChange={handleSelect}
+          isMulti={true}
+          options={options}
+        />
+        {/* <FormHelperText>We'll never share your location.</FormHelperText> */}
+      </FormControl>
+
+      <FormControl>
+        <FormLabel htmlFor="password" fontWeight={"normal"} mt="2%">
+          Picture
+        </FormLabel>
+        <Input variant="filled" onChange={handleImageAsFile} type="file" />
+      </FormControl>
+    </>
+  );
+};
+const Form2 = () => {
+  return (
+    <>
+      <Heading w="100%" textAlign={"center"} fontWeight="normal" mb="2%">
+         Contacts
+      </Heading>
+      <FormControl mr="5%">
+        <FormLabel htmlFor="first-name" fontWeight={"normal"}>
+          Restaurant email
+        </FormLabel>
+        <Input id="first-name"  type="email" placeholder="restaurant@gmail.com" />
+      </FormControl>
+
+      <FormControl>
+        <FormLabel htmlFor="last-name" fontWeight={"normal"}>
+          Restaurant phone
+        </FormLabel>
+        <InputGroup size="sm">
+          <InputLeftAddon
+            bg="gray.50"
+            _dark={{
+              bg: "gray.800",
+            }}
+            color="gray.500"
+            rounded="md"
+          >
+            +254
+          </InputLeftAddon>
+          <Input
+            type="tel"
+            placeholder="711651196"
+            focusBorderColor="brand.400"
+            rounded="md"
+          />
+        </InputGroup>
+    
+      </FormControl>
+
+      <FormControl mt="2%">
+        <FormLabel htmlFor="email" fontWeight={"normal"}>
+          Location
+        </FormLabel>
+        <Input id="email" type="email" />
+        {/* <FormHelperText>We'll never share your email.</FormHelperText> */}
+      </FormControl>
+    </>
+  );
+};
+const Form3 = () => {
+  return (
+    <>
+      <Heading w="100%" textAlign={"center"} fontWeight="normal">
+        Socials
+      </Heading>
+      <SimpleGrid columns={1} spacing={6}>
+        <FormControl as={GridItem} colSpan={[3, 2]}>
+          <FormLabel
+            fontSize="sm"
+            fontWeight="md"
+            color="gray.700"
+            _dark={{
+              color: "gray.50",
+            }}
+          >
+            Website
+          </FormLabel>
+          <InputGroup size="sm">
+            <InputLeftAddon
+              bg="gray.50"
+              _dark={{
+                bg: "gray.800",
+              }}
+              color="gray.500"
+              rounded="md"
+            >
+              http://
+            </InputLeftAddon>
+            <Input
+              type="tel"
+              placeholder="www.example.com"
+              focusBorderColor="brand.400"
+              rounded="md"
+            />
+          </InputGroup>
+        </FormControl>
+        <FormControl as={GridItem} colSpan={[3, 2]}>
+          <FormLabel
+            fontSize="sm"
+            fontWeight="md"
+            color="gray.700"
+            _dark={{
+              color: "gray.50",
+            }}
+          >
+            Instagram
+          </FormLabel>
+          <InputGroup size="sm">
+            <InputLeftAddon
+              bg="gray.50"
+              _dark={{
+                bg: "gray.800",
+              }}
+              color="gray.500"
+              rounded="md"
+            >
+            @
+            </InputLeftAddon>
+            <Input
+              type="tel"
+              placeholder="instagram username"
+              focusBorderColor="brand.400"
+              rounded="md"
+            />
+          </InputGroup>
+        </FormControl>
+
+        <FormControl id="email" mt={1}>
+          <FormLabel
+            fontSize="sm"
+            fontWeight="md"
+            color="gray.700"
+            _dark={{
+              color: "gray.50",
+            }}
+          >
+            About
+          </FormLabel>
+          <Textarea
+            placeholder="you@example.com"
+            rows={3}
+            shadow="sm"
+            focusBorderColor="brand.400"
+            fontSize={{
+              sm: "sm",
+            }}
+          />
+          <FormHelperText>
+            Brief description for your profile. URLs are hyperlinked.
+          </FormHelperText>
+        </FormControl>
+      </SimpleGrid>
+    </>
+  );
+};
 const AddItemModal: React.FC<Props> = () => {
   const initialRef = useRef();
   const { isOpen, onOpen, onClose } = useDisclosure();
-
+  const [show, setShow] = React.useState(false);
+  const handleClick = () => setShow(!show);
+  const toast = useToast();
+  const [step, setStep] = useState(1);
+  const [progress, setProgress] = useState(33.33);
   const [name, setName] = useState("");
-   const [category, setCategory] = useState<OptionType>();
+  const [category, setCategory] = useState<OptionType>();
   const [price, setPrice] = useState("");
   const [amount, setAmount] = useState("");
-    const [imageAsFile, setImageAsFile] = useState<Uint8Array| undefined>();
+  const [imageAsFile, setImageAsFile] = useState<Uint8Array | undefined>();
   const [category_id, setCategoryId] = useState(null);
-    const [selectedOptions, setSelectedOptions] = useState();
-  const toast = useToast();
+  const [selectedOptions, setSelectedOptions] = useState();
 
   const flushInputs = () => {
     setName("");
     setPrice("");
     setAmount("");
   };
-  function handleSelect(data:any) {
+  function handleSelect(data: any) {
     setSelectedOptions(data);
-    
-    console.log('data->',data)
-  }
-    const handleImageAsFile = (e:any) => {
-      const image = e.target.files[0];
-      setImageAsFile((imageFile) => image);
-    };
-  const onOpenDealModal = () => {
-    // if (!user?.sub) {
-    //   //return toast
-    //   return 1;
-    // }
 
+    console.log("data->", data);
+  }
+  const handleImageAsFile = (e: any) => {
+    const image = e.target.files[0];
+    setImageAsFile((imageFile) => image);
+  };
+  const onOpenDealModal = () => {
     onOpen();
   };
   const {
@@ -86,62 +299,58 @@ const AddItemModal: React.FC<Props> = () => {
     formState: { errors },
   } = useForm<RestaurantData>();
   const onSubmit = async (data: RestaurantData) => {
-    console.log("categories", setCategory);
-    if(imageAsFile){
-                const mountainsRef = ref(storage, "mountains.jpg");
+    if (imageAsFile) {
+      const mountainsRef = ref(storage, "mountains.jpg");
 
-                // Create a reference to 'images/mountains.jpg'
-                const mountainImagesRef = ref(storage, "images/mountains.jpg");
+      // Create a reference to 'images/mountains.jpg'
+      const mountainImagesRef = ref(storage, "images/mountains.jpg");
 
-                // While the file names are the same, the references point to different files
-                mountainsRef.name === mountainImagesRef.name; // true
-                mountainsRef.fullPath === mountainImagesRef.fullPath;
-                const uploadTask = uploadBytesResumable(
-                  mountainImagesRef,
-                  imageAsFile
-                );
-                uploadTask.on(
-                  "state_changed",
-                  (snapshot) => {
-                    // Observe state change events such as progress, pause, and resume
-                    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-                    const progress =
-                      (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-                    console.log("Upload is " + progress + "% done");
-                    switch (snapshot.state) {
-                      case "paused":
-                        console.log("Upload is paused");
-                        break;
-                      case "running":
-                        console.log("Upload is running");
-                        break;
-                    }
-                  },
-                  (error) => {
-                    // Handle unsuccessful uploads
-                  },
-                  () => {
-                    // Handle successful uploads on complete
-                    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-                    getDownloadURL(uploadTask.snapshot.ref).then(
-                      async (downloadURL) => {
-                        console.log("File available at", downloadURL);
+      // While the file names are the same, the references point to different files
+      mountainsRef.name === mountainImagesRef.name; // true
+      mountainsRef.fullPath === mountainImagesRef.fullPath;
+      const uploadTask = uploadBytesResumable(mountainImagesRef, imageAsFile);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          // Observe state change events such as progress, pause, and resume
+          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+          }
+        },
+        (error) => {
+          // Handle unsuccessful uploads
+        },
+        () => {
+          // Handle successful uploads on complete
+          // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            console.log("File available at", downloadURL);
 
-                        const timestamp: string = Date.now().toString();
-                        const restaurantCollections = doc(Firestore, "restaurants", timestamp);
+            const timestamp: string = Date.now().toString();
+            const restaurantCollections = doc(
+              Firestore,
+              "restaurants",
+              timestamp
+            );
 
-                        const docsSnap = await setDoc(restaurantCollections, {
-                          name: data?.name,
-                          category: selectedOptions,
-                          image_url: downloadURL,
-                        });
-                      }
-                    );
-                  }
-                );
-
+            const docsSnap = await setDoc(restaurantCollections, {
+              name: data?.name,
+              category: selectedOptions,
+              image_url: downloadURL,
+            });
+          });
+        }
+      );
     }
-
   };
 
   return (
@@ -165,7 +374,7 @@ const AddItemModal: React.FC<Props> = () => {
             <ModalHeader>Add Restaurant</ModalHeader>
             <ModalCloseButton />
             <ModalBody>
-              <FormControl isRequired>
+              {/* <FormControl isRequired>
                 <FormLabel>name</FormLabel>
                 <Input
                   autoFocus
@@ -177,13 +386,7 @@ const AddItemModal: React.FC<Props> = () => {
               </FormControl>
               <FormControl isRequired mt={6}>
                 <FormLabel>Categories</FormLabel>
-                {/* <Input
-                  autoFocus
-                  variant="filled"
-                  {...register("categories")}
-                  placeholder="categories"
-                  type="text"
-                /> */}
+     
                 <Creatable
                   value={selectedOptions}
                   onChange={handleSelect}
@@ -199,30 +402,83 @@ const AddItemModal: React.FC<Props> = () => {
                   onChange={handleImageAsFile}
                   type="file"
                 />
-              </FormControl>
-
-              {/* <FormControl mt={4}>
-                <FormLabel htmlFor="category">Category</FormLabel>
-                <Select
-                  {...register("category_id", { required: true })}
-                  name="category_id"
-                  id="category"
-                  onChange={(e) => setCategoryId(e.target.value)}
-                  placeholder="Select Category"
-                >
-                  {data &&
-                    data.categories.map(({ id, name }) => (
-                      <option key={id} value={id}>
-                        {name}
-                      </option>
-                    ))}
-                </Select>
-                <FormErrorMessage></FormErrorMessage>
+                
               </FormControl> */}
+              <Box
+                // borderWidth="1px"
+                rounded="lg"
+                // shadow="1px 1px 3px rgba(0,0,0,0.3)"
+                // maxWidth={800}
+                p={6}
+                m="10px auto"
+                as="form"
+              >
+                <Progress
+                  hasStripe
+                  value={progress}
+                  mb="5%"
+                  mx="5%"
+                  isAnimated
+                ></Progress>
+                {step === 1 ? <Form1 /> : step === 2 ? <Form2 /> : <Form3 />}
+              </Box>
             </ModalBody>
 
             <ModalFooter>
-              <Button mr={3} onClick={onClose}>
+              <ButtonGroup mt="5%" w="100%">
+                <Flex w="100%" justifyContent="space-between">
+                  <Flex>
+                    <Button
+                      onClick={() => {
+                        setStep(step - 1);
+                        setProgress(progress - 33.33);
+                      }}
+                      isDisabled={step === 1}
+                      colorScheme="teal"
+                      variant="solid"
+                      w="7rem"
+                      mr="5%"
+                    >
+                      Back
+                    </Button>
+                    <Button
+                      w="7rem"
+                      isDisabled={step === 3}
+                      onClick={() => {
+                        setStep(step + 1);
+                        if (step === 3) {
+                          setProgress(100);
+                        } else {
+                          setProgress(progress + 33.33);
+                        }
+                      }}
+                      colorScheme="teal"
+                      variant="outline"
+                    >
+                      Next
+                    </Button>
+                  </Flex>
+                  {step === 3 ? (
+                    <Button
+                      w="7rem"
+                      colorScheme="red"
+                      variant="solid"
+                      onClick={() => {
+                        toast({
+                          title: "Account created.",
+                          description: "We've created your account for you.",
+                          status: "success",
+                          duration: 3000,
+                          isClosable: true,
+                        });
+                      }}
+                    >
+                      Submit
+                    </Button>
+                  ) : null}
+                </Flex>
+              </ButtonGroup>
+              {/* <Button mr={3} onClick={onClose}>
                 Close
               </Button>
               <Button
@@ -234,7 +490,7 @@ const AddItemModal: React.FC<Props> = () => {
                 ml={3}
               >
                 Create
-              </Button>
+              </Button> */}
             </ModalFooter>
           </form>
         </ModalContent>
